3de813379f47c943955384e6907194775368942a
Canary-Prism <canaryprsn@gmail.com>
2024-10-03 00:12:42 +0800

    update to use inherited implementation

--- src/main/java/canaryprism/dbc/swing/DiscordView.java
+++ src/main/java/canaryprism/dbc/swing/DiscordView.java
@@ -47,83 +47,7 @@ public class DiscordView extends JComponent {
         var channel_list = new ServerListView(api) {
             @Override
             protected JComponent createServerItemView(Server server) {
-                var view = new JComponent() {
-
-                    private Image icon;
-
-                    static final int pfp = 45;
-
-                    {
-                        // Thread.ofVirtual().start(() -> {
-                            mewo: try {
-                                if (server.getIcon().isEmpty()) {
-                                    break mewo;
-                                }
-                                icon = MediaCache.getImage("server_icon", server, (e) -> e.getIcon().get().getUrl());
-                                repaint();
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        // });
-                    }
-
-
-                    @Override
-                    public Dimension getPreferredSize() {
-                        return new Dimension(pfp, pfp);
-                    }
-
-                    @Override
-                    public Dimension getMaximumSize() {
-                        return getPreferredSize();
-                    }
-
-                    @Override
-                    protected void paintComponent(Graphics g) {
-                        super.paintComponent(g);
-                        {
-                            var g2 = (Graphics2D) g.create();
-                            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-                            g2.clip(new Ellipse2D.Double(0, 0, pfp, pfp));
-
-                            if (icon != null) {
-                                g2.drawImage(icon, 0, 0, pfp, pfp, this);
-                            } else {
-                                g2.setColor(java.awt.Color.GRAY);
-                                g2.fill(new Ellipse2D.Double(0, 0, pfp, pfp));
-
-                                g2.setColor(getForeground());
-
-                                // draw the initials of the server name, this'll be a bit tricky
-                                var name = server.getName();
-                                var sb = new StringBuilder();
-                                Stream.of(TextView.whitespace.split(name)) // just steal the pattern from TextView, it's fine
-                                    .map((e) -> e.charAt(0))
-                                    .forEach(sb::append);
-
-                                var initials = sb.toString();
-
-                                var string_width = g2.getFontMetrics().stringWidth(initials) + 5;
-
-                                var scale = pfp / (float) string_width;
-
-                                g2.scale(scale, scale);
-
-                                g2.drawString(initials, 2.5f, (string_width) / 2f + g2.getFontMetrics().getHeight() / 2f - g2.getFontMetrics().getDescent());
-                            }
-                        }
-                    }
-                };
-
-                var panel = new JPanel();
-
-                // panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-
-                // view.setLayout(new FlowLayout());
-                panel.setBorder(new EmptyBorder(2, 2, 2, 2));
-
-                view.setToolTipText(server.getName());
+                var view = super.createServerItemView(server);
 
                 // view.setBorder(new LineBorder(Color.red, 1));
 
@@ -134,10 +58,8 @@ public class DiscordView extends JComponent {
                         showServer(server);
                     }
                 });
-                
-                panel.add(view);
 
-                return panel;
+                return view;
             }
         };
 
