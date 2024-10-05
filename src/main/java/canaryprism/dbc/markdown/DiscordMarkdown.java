2e16aac8cdd11ba066e8fe432a56c289e00e554e
Canary-Prism <canaryprsn@gmail.com>
2024-10-05 14:43:29 +0800

    escape url to prevent problems with weird urls

--- src/main/java/canaryprism/dbc/markdown/DiscordMarkdown.java
+++ src/main/java/canaryprism/dbc/markdown/DiscordMarkdown.java
@@ -3,6 +3,8 @@ package canaryprism.dbc.markdown;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
+import org.apache.commons.text.StringEscapeUtils;
+
 import canaryprism.dbc.Main;
 
 public class DiscordMarkdown {
@@ -287,7 +289,7 @@ public class DiscordMarkdown {
         record Link(String url, String text) implements Element {
             @Override
             public String stringify() {
-                return String.format("<link url=\"%s\">%s</link>", url, text);
+                return String.format("<link escaped_url=\"%s\">%s</link>", StringEscapeUtils.escapeXml11(url), text);
             }
         }
 
