dc844f9067aff6a7031f6f83d3d5e99d024128cf
Canary-Prism <canaryprsn@gmail.com>
2025-04-17 10:39:15 +0900

    add modules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module canaryprism.dbc {
+    requires com.formdev.flatlaf;
+    requires dev.dirs;
+    requires org.apache.commons.io;
+    requires org.apache.commons.text;
+    requires org.javacord.api;
+    requires org.json;
+}
