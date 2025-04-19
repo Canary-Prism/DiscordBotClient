43089576c72a605a7432bd23005b32cd1f09e6f2
Canary-Prism <canaryprsn@gmail.com>
2025-04-19 09:09:04 +0900

    change Emoji to CustomEmoji bc otherwise it'd include unicode emojis which are just encoded as unicode emojis in text and not a custom emoji yk what i'm talking about

---
+++ src/main/java/canaryprism/dbc/markdown/text/data/CustomEmoji.java
@@ -0,0 +1,5 @@
+package canaryprism.dbc.markdown.text.data;
+
+public record CustomEmoji(net.dv8tion.jda.api.entities.emoji.CustomEmoji emoji)  {
+
+}
--- src/main/java/canaryprism/dbc/markdown/text/data/Emoji.java
+++
@@ -1,5 +0,0 @@
-package canaryprism.dbc.markdown.text.data;
-
-public record Emoji(org.javacord.api.entity.emoji.Emoji emoji)  {
-
-}
